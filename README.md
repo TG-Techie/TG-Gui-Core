58117412614b1191b2089bccd7c000ba2b2579b8
Jonah Yolles-Murphy <TGTechie01@gmial.com>
2021-01-14 01:14:34 -0500

    formatted

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # TG-Gui-Core
-The core module used for TG-Gui, used to build implementations of TG-Gui standard library.
+The core module of TG-Gui, used to build implementations of TG-Gui standard library.
--- tg_gui_core/base.py
+++ tg_gui_core/base.py
@@ -1,6 +1,29 @@
+# The MIT License (MIT)
+#
+# Copyright (c) 2021 Jonah Yolles-Murphy (TG-Techie)
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
 import sys
+
 sys_impl_name_ = sys.implementation.name
-on_circuitpython_ = (sys_impl_name_ in ('circuitpython', 'micropython'))
+on_circuitpython_ = sys_impl_name_ in ("circuitpython", "micropython")
 
 import random as _random
 
@@ -8,88 +31,106 @@ from .layout_classes import *
 from .position_specifiers import *
 from .dimension_specifiers import *
 
+
 class PlacementError(Exception):
     pass
+
 
 class RenderError(Exception):
     pass
 
+
 _next_id = _random.randint(0, 11)
+
+
 def uid():
     global _next_id
     id = _next_id
     _next_id += 1
     return id
 
+
 def clip(lower, value, upper):
     return min(max(lower, value), upper)
 
-class align: # for text alignment
-    leading  = uid()
-    center   = uid()
+
+class align:  # for text alignment
+    leading = uid()
+    center = uid()
     trailing = uid()
+
 
 class color:
     # TODO: consider make ready only using proerties and lambdas vs performance
-    _clear   = None # tentative
-    red     = 0xff0000
-    orange  = 0xffa500
-    yellow  = 0xffff00
-    green   = 0x00ff00
-    blue    = 0x0000ff
-    purple  = 0xcc8899
+    _clear = None  # tentative
+    red = 0xFF0000
+    orange = 0xFFA500
+    yellow = 0xFFFF00
+    green = 0x00FF00
+    blue = 0x0000FF
+    purple = 0xCC8899
 
-    white     = 0xffffff
-    lightgray = 0xc4c4c4
-    gray      = 0x909090
-    darkgray  = 0x606060
-    black     = 0x000000
+    white = 0xFFFFFF
+    lightgray = 0xC4C4C4
+    gray = 0x909090
+    darkgray = 0x606060
+    black = 0x000000
 
-class Palette():
 
-    def __init__(self, *,
-        fill_color,     text_color,
-        selected_text,  selected_fill,
+class Palette:
+    def __init__(
+        self,
+        *,
+        fill_color,
+        text_color,
+        selected_text,
+        selected_fill,
         backfill,
     ):
-        self.fill_color    = fill_color
+        self.fill_color = fill_color
         self.selected_fill = selected_fill
-        self.text_color    = text_color
+        self.text_color = text_color
         self.selected_text = selected_text
         self.backfill = backfill
 
-class Palettes():
 
-    def __init__(self, *,
-        primary, secondary
-    ):
+class Palettes:
+    def __init__(self, *, primary, secondary):
         self.primary = primary
         self.secondary = secondary
 
-class Defaults():
 
-    def __init__(self, *,
-        margin, radius, font_size,
-        _fill_color_,   _selected_fill_,
-        _text_color_,   _selected_text_,
+class Defaults:
+    def __init__(
+        self,
+        *,
+        margin,
+        radius,
+        font_size,
+        _fill_color_,
+        _selected_fill_,
+        _text_color_,
+        _selected_text_,
     ):
-        self.margin     = margin
-        self.radius     = radius
-        self.font_size  = font_size
+        self.margin = margin
+        self.radius = radius
+        self.font_size = font_size
 
-        self._fill_color_    = _fill_color_
+        self._fill_color_ = _fill_color_
         self._selected_fill_ = _selected_fill_
-        self._text_color_    = _text_color_
+        self._text_color_ = _text_color_
         self._selected_text_ = _selected_text_
 
-class Screen():
 
-    def __init__(self, *,
+class Screen:
+    def __init__(
+        self,
+        *,
         min_size,
         palettes: Palettes,
         default: Defaults,
         layout_class: LayoutCls,
-        outer:'Screen'=None,
+        outer: "Screen" = None,
     ):
         self._id_ = uid()
         self.default = default
@@ -103,40 +144,45 @@ class Screen():
         return f"<{type(self).__name__} {self._id_}>"
 
     def __getattr__(self, name):
-        #if name in self._kwargs:
-            #return self._kwargs[name]
+        # if name in self._kwargs:
+        # return self._kwargs[name]
         if self.outer is not None:
-            try:    return getattr(self.outer, name)
-            except: pass
+            try:
+                return getattr(self.outer, name)
+            except:
+                pass
         raise AttributeError(f"unable to get attribute `.{name}`")
 
-    def on_widget_nest_in(self, wid:'Widget'):
+    def on_widget_nest_in(self, wid: "Widget"):
         pass
 
-    def on_widget_render(self, wid:'Widget'):
+    def on_widget_render(self, wid: "Widget"):
         pass
 
-    def on_widget_derender(self, wid:'Widget'):
+    def on_widget_derender(self, wid: "Widget"):
         pass
 
-    def on_container_place(self, wid:'Widget'):
+    def on_container_place(self, wid: "Widget"):
         pass
 
-    def on_container_pickup(self, wid:'Widget'):
+    def on_container_pickup(self, wid: "Widget"):
         pass
 
-class Widget():
+
+class Widget:
 
     _next_id = 0
 
-    def __init__(self, *, superior=None, margin=None): # TODO: should use nest or superior kwarg?
+    def __init__(
+        self, *, superior=None, margin=None
+    ):  # TODO: should use nest or superior kwarg?
         global Widget
         self._id_ = uid()
 
-        self._superior_ = None # type: Optional[superior]
+        self._superior_ = None  # type: Optional[superior]
         self._screen_ = None
 
-        self._placement_ = None # type: Optional[Tuple[x, y, width, height]]
+        self._placement_ = None  # type: Optional[Tuple[x, y, width, height]]
         self._rel_placement_ = None
         self._phys_coord = None
 
@@ -153,12 +199,10 @@ class Widget():
         return self._superior_ is not None
 
     def isnestedin(self, maybe_superior):
-        return ( self._superior_ is maybe_superior
-            and self in maybe_superior._nested_
-        )
+        return self._superior_ is maybe_superior and self in maybe_superior._nested_
 
     def isplaced(self):
-        return self._placement_ is not None # and self.isnested()
+        return self._placement_ is not None  # and self.isnested()
 
     def isrendered(self):
         return self._rendered_
@@ -174,8 +218,7 @@ class Widget():
             pass
         else:
             raise ValueError(
-                 f"{self} already nested in {current}, "
-                +f"cannot nest in {superior}"
+                f"{self} already nested in {current}, " + f"cannot nest in {superior}"
             )
 
         self._on_nest_()
@@ -203,10 +246,10 @@ class Widget():
         # is there a diference between place and render? (yes, button state)
         assert self.isnested(), f"{self} must be nested to place it"
         # print(self)
-        #assert not self.isplaced(), f"cannot doulbe place {self}, it must be pickup-ed first"
+        # assert not self.isplaced(), f"cannot doulbe place {self}, it must be pickup-ed first"
 
         was_on_screen = self.isrendered()
-        if was_on_screen: #if was_on_screen := self.isrendered()
+        if was_on_screen:  # if was_on_screen := self.isrendered()
             self._derender_()
 
         # get margin
@@ -221,9 +264,8 @@ class Widget():
         if isinstance(height, DimensionSpecifier):
             height = height._calc_dim_(self)
 
-
         # make sure PositionSpecifiers have access to width/height
-        self._placement_ =  (None, None, width, height)
+        self._placement_ = (None, None, width, height)
         # print(self, self.width)
 
         # format coord
@@ -239,25 +281,31 @@ class Widget():
             # print('y is posspec')
             y = y._calc_y_(self)
 
-        if self._superior_ is None and (x < 0 or y < 0): raise ValueError(
-            f"right aligned coord cannot be used with root widgets"
-        )
+        if self._superior_ is None and (x < 0 or y < 0):
+            raise ValueError(f"right aligned coord cannot be used with root widgets")
 
         # adjust
-        if x < 0: x = self._superior_.width - width + 1 + x
-        if y < 0: y = self._superior_.height - height + 1 + y
+        if x < 0:
+            x = self._superior_.width - width + 1 + x
+        if y < 0:
+            y = self._superior_.height - height + 1 + y
 
         # save
         self._placement_ = placement = x, y, w, h = (x, y, width, height)
 
         # calc relative placement
-        self._rel_placement_ = rel_placement = (mar+x, mar+y, w-(2*mar), h-(2*mar))
+        self._rel_placement_ = rel_placement = (
+            mar + x,
+            mar + y,
+            w - (2 * mar),
+            h - (2 * mar),
+        )
         rx, ry, rw, rh = rel_placement
 
         # calc absolute physical placement
         supx, supy = self._superior_._phys_coord_
-        self._phys_coord_ = (supx+rx, supy+ry)
-        self._phys_end_coord = (supx+rx+rw, supy+ry+rh)
+        self._phys_coord_ = (supx + rx, supy + ry)
+        self._phys_end_coord = (supx + rx + rw, supy + ry + rh)
 
         if was_on_screen:
             self._render_()
@@ -277,7 +325,6 @@ class Widget():
         self._rendered_ = True
         self._screen_.on_widget_render(self)
 
-
     def _derender_(self):
         self._screen_.on_widget_derender(self)
         self._rendered_ = False
@@ -295,38 +342,37 @@ class Widget():
         self._rel_placement_ = None
         self._phys_coord = None
 
-    # #@micropython.native
     def _has_phys_coord_in_(self, coord):
-        #print(f"self._phys_coord_={self._phys_coord_}, coord={coord}, self._phys_end_coord={self._phys_end_coord}")
+        # print(f"self._phys_coord_={self._phys_coord_}, coord={coord}, self._phys_end_coord={self._phys_end_coord}")
         minx, miny = self._phys_coord_
         x, y = coord
         maxx, maxy = self._phys_end_coord
         return (minx <= x <= maxx) and (miny <= y <= maxy)
 
     # coordinates and dimension getters
-    coord     = property(lambda self: self._placement_[0:2])
+    coord = property(lambda self: self._placement_[0:2])
     _rel_coord_ = property(lambda self: self._rel_placement_[0:2])
     # uses raw exposed tuple # _phys_coord_ = property(lambda self: self._phys_coord)
 
-    dims     = property(lambda self: self._placement_[2:4])
+    dims = property(lambda self: self._placement_[2:4])
     _phys_dims_ = property(lambda self: self._rel_placement_[2:4])
 
-    x       = property(lambda self: self._placement_[0])
+    x = property(lambda self: self._placement_[0])
     _rel_x_ = property(lambda self: self._rel_placement_[0])
     _phys_x_ = property(lambda self: self._phys_coord_[0])
 
-    y       = property(lambda self: self._placement_[1])
+    y = property(lambda self: self._placement_[1])
     _rel_y_ = property(lambda self: self._rel_placement_[1])
     _phys_y_ = property(lambda self: self._phys_coord_[1])
 
-    width     = property(lambda self: self._placement_[2])
+    width = property(lambda self: self._placement_[2])
     _phys_width_ = property(lambda self: self._rel_placement_[2])
 
-    height     = property(lambda self: self._placement_[3])
+    height = property(lambda self: self._placement_[3])
     _phys_height_ = property(lambda self: self._rel_placement_[3])
 
-class Container(Widget):
 
+class Container(Widget):
     def __init__(self, superior=None):
         global Widget
 
@@ -341,49 +387,50 @@ class Container(Widget):
         return ((0, 0), self.dims)
 
     def _setup_(self):
-        pass # used for setting up of reuables contianers "compound widgets"
+        pass  # used for setting up of reuables contianers "compound widgets"
 
-    def _nest_(self, wid:Widget):
+    def _nest_(self, wid: Widget):
         if wid not in self._nested_:
             self._nested_.append(wid)
             wid._nest_in_(self)
 
-    def _unnest_(self, wid:Widget):
+    def _unnest_(self, wid: Widget):
         raise NotImplementedError("dev side not implemented")
 
     def _place_(self, coord, dims):
         raise NotImplementedError(f"{type(self).__name__}._place_ not implemented")
         # suggested code:
-        '''
+        """
         Widget._place_(self, coord, dims)
         `nested widget placement code`
         self._screen_.on_container_place(self)
-        '''
+        """
 
     def _pickup_(self):
         raise NotImplementedError(f"{type(self).__name__}._pickup_ not implemented")
         # suggested code:
-        '''
+        """
         self._screen_.on_container_pickup(self)
         `nested widget pickup code`
         Widget._pickup_(self)
-        '''
+        """
 
     def _render_(self):
         raise NotImplementedError(f"{type(self).__name__}._render_ not implemented")
         # suggested code:
-        '''
+        """
         super()._render_()
         self._render_nested_()
-        '''
+        """
 
     def _derender_(self):
         raise NotImplementedError(f"{type(self).__name__}._derender_ not implemented")
         # suggested code:
-        '''
+        """
         super()._derender_()
         self._derender_nested_()
-        '''
+        """
+
     #
     # def _render_nested_(self):
     #     raise NotImplementedError(f"{type(self).__name__}._render_nested_ not implemented")
